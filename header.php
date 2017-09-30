e42e6327b1b54b203a2a8ad6d092db87623c75b2
Nick Fuller <nfuller52@gmail.com>
2017-09-30 14:18:16 -0700

    Add page title to header
    Add template for home page

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title><?php ?></title>
+        <title><?php bloginfo( 'name' ) . wp_title( '&#45;' ); ?></title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <?php wp_head(); ?>
     </head>
-<body>
+<body <?php body_class(); ?>>
